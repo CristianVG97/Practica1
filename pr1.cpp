4cc96eac68b1c93fa1df2b8f63e9a8156ac0d83c
CristianVG97 <crisg.3080@gmail.com>
2017-10-20 11:15:40 -0500

    primer entrega

---
+++ pr1.cpp
@@ -0,0 +1,13 @@
+// pr1.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hola mundo";
+    return 0;
+}
+
